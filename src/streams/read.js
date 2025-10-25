72e2046e80ac748c8d9a9a6f7f673269d771e340
RamanMilashevich <roma.milashevich@gmail.com>
2025-10-25 21:43:27 +0200

    feat: implement streams operations for file I/O and text transformation

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { createReadStream } from 'node:fs';
+import { join } from 'node:path';
+
 const read = async () => {
-  // Write your code here
+  const filePath = join(process.cwd(), 'src', 'streams', 'files', 'fileToRead.txt');
+  
+  // let's create a readable stream and pipe it to stdout
+  const stream = createReadStream(filePath);
+  stream.pipe(process.stdout);
 };
 
 await read();
